f82eacb71d6b1820f0ae1340d761339f88d79b4f
Ritesh230087 <riteshdaas01@gmail.com>
2025-06-11 23:53:06 +0545

    Login Page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import LoginForm from '../components/auth/LoginForm'
+import { AuthContext } from '../auth/AuthProvider'
+
+export default function Login() {
+    const { user } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const returnToHome = (event) => {
+        event.preventDefault()
+        navigate("/")
+    }
+
+    if (user) {
+        return <div>You are already logged in.</div>
+    }
+
+    return (
+        <div>
+            <div>Login</div>
+            <NavLink to="/">Go back</NavLink>
+            <Link to="/register">Register</Link>
+            <button onClick={returnToHome}>Button Click</button>
+            <button onClick={(event) => returnToHome(event)}>Button Click Callback</button>
+
+            <div>
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
