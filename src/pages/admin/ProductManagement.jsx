809af70c5f93a261278ca86e80aad617aa40cfd2
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-17 02:00:00 +0545

    product management

--- src/pages/admin/ProductManagement.jsx
+++ src/pages/admin/ProductManagement.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { useProduct } from "../../hooks/admin/useProduct";
 import { fetchCategories } from "/src/services/admin/categoryService";
