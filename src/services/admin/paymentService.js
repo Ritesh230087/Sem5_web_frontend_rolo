fa4445ae1ee3744a4a0c22626ddec429cc612f55
Ritesh230087 <riteshdaas01@gmail.com>
2025-07-17 02:14:07 +0545

    payment service

--- src/services/admin/paymentService.js
+++ src/services/admin/paymentService.js
@@ -5,9 +5,3 @@ export const initiateEsewa = (orderId, amount) =>
 
 export const verifyEsewa = (transactionId, orderId, amt) =>
   axios.post('/api/payments/esewa/verify', { transactionId, orderId, amt });
-
-export const initiateKhalti = (orderId, amount) =>
-  axios.post('/api/payments/khalti/initiate', { orderId, totalAmount: amount });
-
-export const verifyKhalti = (token, amount, orderId) =>
-  axios.post('/api/payments/khalti/verify', { token, amount, orderId });
